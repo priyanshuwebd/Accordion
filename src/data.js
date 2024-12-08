d3682324bbfaa7427314cb93e8b0dd606b5b79a5
priyanshupande <priyanshupande23@gmail.com>
2024-12-08 14:35:12 +0530

    accorion uplode

---
+++ src/data.js
@@ -0,0 +1,35 @@
+const question = [
+{
+    id:1,
+    title: "Do I have to allow the use of cookies on this website?",
+    info: "You’re not required to accept cookies, but some features may not work as expected without them. Cookies help personalize your experience and remember your preferences. You can usually manage cookie settings in your browser."
+
+},
+
+{
+    id:2,
+    title: "How do I change my account password?",
+    info: "Go to the Account Settings or Profile section of the website. Look for an option labeled Change Password or Security Settings. Follow the prompts to enter your current password and set a new one. Make sure to use a strong password that’s hard to guess!",
+
+},
+
+{
+    id:3,
+    title: "Why am I being asked to verify my email address?",
+    info: "Email verification ensures that the email associated with your account is active and belongs to you. This helps with account security and allows the website to contact you with important updates. Check your inbox for a verification email and follow the instructions to confirm.",
+
+},
+
+{
+    id:4,
+    title: "How can I update my personal information on my profile?",
+    info: "Head to the Profile or Account Settings section. Here, you should find fields to update your personal information, such as name, email, and phone number. Don’t forget to save your changes after updating."
+},
+
+{
+    id:5,
+    title: "Why am I being asked to enable JavaScript on this site?",
+    info: "JavaScript enables interactive features on many websites, such as forms, dynamic content, and animations. If JavaScript is disabled, some features might not work properly. To enable it, go to your browser settings and turn on JavaScript."
+},
+]
+export default question;
